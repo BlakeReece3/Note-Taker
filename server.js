022b48791c74ef4f109cc09517d701283a6503de
Blake Reece <frankiereece3@gmail.com>
2023-03-16 21:35:25 -0700

    added port listener

--- server.js
+++ server.js
@@ -1,21 +1,27 @@
 const express = require('express');
 const fs = require('fs');
-const { dirname } = require('path');
 const path = require('path');
 const db = require('./db/db.json');
 
 const app = express();
 const PORT = process.env.port || 3001;
 
-// Makes Home page display the index file
+// Route for notes.html
+app.get('/notes', (req, res) => {res.sendFile(path.join(__dirname, '/public/notes.html'))});
+// Route for homepage
 app.get('/', (req, res) => res.sendFile(path.join(--dirname, '/public/index.html')));
-// Makes Notes page display notes file
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-});
+
 // Parsing json/app
 app.use(express.json());
 // Static middleware for assets in public folder
 app.use(express.static('public'));
 
 app.use(express.urlencoded({ extended: true }));
+
+
+
+
+
+app.listen(PORT, () => {
+    console.log(`Example app listening at http://localhost:${PORT}`);
+  });
