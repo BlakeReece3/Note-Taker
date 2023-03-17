63d3c5f6738705acefb4428fe3f14aa6b8a25760
Blake Reece <frankiereece3@gmail.com>
2023-03-16 20:44:38 -0700

    node modules/package.json

--- server.js
+++ server.js
@@ -7,3 +7,15 @@ const db = require('./db/db.json');
 const app = express();
 const PORT = process.env.port || 3001;
 
+// Makes Home page display the index file
+app.get('/', (req, res) => res.sendFile(path.join(--dirname, '/public/index.html')));
+// Makes Notes page display notes file
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/notes.html'))
+});
+// Parsing json/app
+app.use(express.json());
+// Static middleware for assets in public folder
+app.use(express.static('public'));
+
+app.use(express.urlencoded({ extended: true }));
