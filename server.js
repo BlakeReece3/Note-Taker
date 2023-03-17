0a08f808b913a9c095abd6b9e88c086427f38b7b
Blake Reece <frankiereece3@gmail.com>
2023-03-16 19:42:31 -0700

    created package.json and public folder

--- server.js
+++ server.js
@@ -7,8 +7,3 @@ const db = require('./db/db.json');
 const app = express();
 const PORT = process.env.port || 3001;
 
-app.get('/', (req, res) => res.sendFile(path.join(--dirname, '/public/index.html')));
-
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, '/public/notes.html'))
-});
