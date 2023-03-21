238894246d943d6947cb6afc230fd98a4767d354
Blake Reece <frankiereece3@gmail.com>
2023-03-20 19:59:44 -0700

    created css folder for styles.css in assets, created js folder for index.js in assets

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.port || 3001;
 // Route for notes.html
 app.get('/notes', (req, res) => {res.sendFile(path.join(__dirname, '/public/notes.html'))});
 // Route for homepage
-app.get('/', (req, res) => res.sendFile(path.join(--dirname, '/public/index.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/index.html')));
 
 // Parsing json/app
 app.use(express.json());
